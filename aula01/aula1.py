3aef56636c0c05fd2c63f412e54c0cf150da1aa1
santossofia <sofia.santos@compasso.com.br>
2022-03-25 14:42:32 -0300

    Commit inicial

---
+++ aula01/aula1.py
@@ -0,0 +1,20 @@
+# Adicionando comentários
+#  Isso é engraçado porque anotei o que será feito aqui usando justamente o que foi ensinado
+
+print('Linha 1')
+print('Linha 2')
+print('Linha 3')
+#  Comentário sobre o que essa linha faz: Imprime na tela o texto 'Linha 4'
+print('Linha 4')  #  Isso é um comentário
+print('Linha 5')
+#  print('Linha 6') Essa parte do código foi removida e virou um comentário
+""""
+Isso se torna um comentário
+print('Linha 7')
+print('Linha 8')
+print('Linha 9')
+print('Linha 10')  
+print('Linha 11')
+"""
+print('Linha 12')
+print('Linha 13')
