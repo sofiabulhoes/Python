24b52354ad0a20f29544eaa4ce2d5897b5559edf
santossofia <sofia.santos@compasso.com.br>
2022-03-28 16:26:48 -0300

    Aulas Python

--- aula03/aula3.py
+++
@@ -1,5 +0,0 @@
-
-print("Essa é uma string bem aqui")
-print('Essa é uma \'string\' bem aqui') #A barra invertida faz o python ignorar o proximo caractere
-print("Essa é uma 'string' bem aqui")  #Usar aspas diferentes
-print(r"Essa é uma \'string'\ bem aqui") # r (raw) no começo faz o python ignorar a formatação da barra invertida, exibindo o texto cru
